a50e7d74151f7ebd708fbd0b610f3d4ba50a111f
Shaun Robinson <shaun@mainstreetgenome.com>
2015-01-16 15:22:45 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject msg/salesforce "1.0.1"
+(defproject msg/salesforce "1.0.2"
   :description "A clojure library for accessing the salesforce api"
   :url "http://owainlewis.com"
   :license {:name "Eclipse Public License"
