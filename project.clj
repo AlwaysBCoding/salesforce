f59aa5aa4d0b9c821610da0c5d7aef4da54129ee
Shaun Robinson <shaun@mainstreetgenome.com>
2015-01-16 15:24:10 -0500

    Add release server info to project.clj

--- project.clj
+++ project.clj
@@ -5,4 +5,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "0.7.2"]
-                 [cheshire "5.1.1"]])
+                 [cheshire "5.1.1"]]
+
+
+  :release-tasks
+  [["vcs" "assert-committed"]
+   ["change" "version" "leiningen.release/bump-version" "release"]
+   ["vcs" "commit"]
+   ["deploy"]
+   ["change" "version" "leiningen.release/bump-version"]
+   ["vcs" "commit"]
+   ["vcs" "push"]]
+  :repositories {"releases"
+                                       {:url "s3p://msg-jars/releases/"
+                                        :username   ~(System/getenv "MSG_AWS_ACCESS_KEY")
+                                        :passphrase ~(System/getenv "MSG_AWS_SECRET_KEY")
+                                        :sign-releases false}
+                 "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
